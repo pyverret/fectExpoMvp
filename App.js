e6646da97da8d5253ec6198b607ee5877c2fcf30
PY Verret <pverret@gmail.com>
2024-04-06 12:33:31 -0400

    Quick MVP

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+// TODO: Use Typescript
+// TODO: Use Typescript Enum
+// TODO: Extract
+const DEFAULT_COLOR = 'white';
+const COLOR_ENUM = ['blue', 'green', 'yellow'];
+const COLOR_LIST = {
+    blue: '#294CBE',
+    green: '#0bda51',
+    yellow: '#ffff66',
+    white: '#ffffff'
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [color, setColor] = useState('white')
+
+    const backgroundColor = {
+        backgroundColor: COLOR_LIST[color]
+    }
+
+    return (
+        <View style={[styles.container, backgroundColor]}>
+            {/* TODO: Extract in it's own component */}
+            <Text>Button to be changed to use web service to push update to the app</Text>
+            {COLOR_ENUM.map((colorName) => <Button onPress={() => setColor(colorName)} title={colorName} />)}
+            <Button onPress={() => setColor(DEFAULT_COLOR)} title='Reset' color="red" />
+        </View>
+    );
+
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: `#fff`,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+How to run the project.
+
+## Prerequisites
+- Node see `.nvmrc` file for version
+    - Recommended to use `nvm` to manage node versions
+- Install expo on your machine `npm install -g create-expo-app`
+- Code editor `Android Studio`
+- A git client, cli or the one built within `Android Studio`
+- Clone the repo ([How to here](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository))
+
+## To start the project
+1. To to the root folder of the repo
+2. Run `npm install` to install the project dependencies
+3. Start a device in android studio `Device Manager` on the right side
+4. `expo start --android`
